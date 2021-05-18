2d23fcce0e8ea9d90a06dd6f56dac234d4011885
Darrienrjohnson <69940765+Darrienrjohnson@users.noreply.github.com>
2021-05-17 23:05:17 -0400

    mercury exercise
    underestand conditional statements

---
+++ apps.js
@@ -0,0 +1,9 @@
+
+const answer = prompt("Which planet is closest to the sun?");
+
+if (answer.toUpperCase() === "MERCURY") {
+  console.log("Thats correct!");
+} else {
+  console.log("Sorry, that's incorrect.");
+}
+
